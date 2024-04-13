fbbefa65d2f64767bbed5d7999f5ec76e096de00
alangabrielordonezbenito <141080438+alangabrielordonezbenito@users.noreply.github.com>
2024-04-13 17:25:00 -0500

    Configurar camara y geolocalización

--- lib/views/camera_screen.dart
+++ lib/views/camera_screen.dart
@@ -1,8 +1,8 @@
+import 'dart:typed_data';
 
-
+import 'package:ansur_app_movil/views/widgets/app_loading.dart';
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
-
 
 class CameraScreen extends StatefulWidget {
   final List<CameraDescription>? cameras;
@@ -14,15 +14,99 @@ class CameraScreen extends StatefulWidget {
 
 class _CameraScreenState extends State<CameraScreen> {
 
+  late CameraController _cameraController;
+
+  @override
+  void dispose() {
+    _cameraController.dispose();
+    super.dispose();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    initCamera(widget.cameras![0]);
+  }
+
+  Future initCamera(CameraDescription cameraDescription) async {
+    _cameraController =
+        CameraController(cameraDescription, ResolutionPreset.high);
+    try {
+      await _cameraController.initialize().then((_) {
+        if (!mounted) return;
+        setState(() {});
+      });
+    } on CameraException catch (e) {
+      debugPrint("camera error $e");
+    }
+  }
+
+  Future<Uint8List?> takePicture() async {
+    if (!_cameraController.value.isInitialized) {
+      return null;
+    }
+    if (_cameraController.value.isTakingPicture) {
+      return null;
+    }
+    try {
+      await _cameraController.setFlashMode(FlashMode.off);
+      final photo = await _cameraController.takePicture();
+      return await photo.readAsBytes();
+    } on CameraException catch (e) {
+      debugPrint('Error occured while taking picture: $e');
+      return null;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-      appBar: AppBar(
-        title: const Text('Datos de compra'),
+    return Scaffold(
+      body: SafeArea(
+        child: Stack(
+          children: [
+            _cameraController.value.isInitialized
+                ? CameraPreview(_cameraController)
+                : const AppLoading(),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Container(
+                height: MediaQuery.of(context).size.height * 0.20,
+                decoration: const BoxDecoration(
+                  borderRadius: BorderRadius.vertical(top: Radius.circular(18)),
+                  color: Color(0xFFEC3138)
+                ),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Expanded(
+                      child: IconButton(
+
+                        icon: const Icon(Icons.circle),
+                        iconSize: 75,
+                        padding: EdgeInsets.zero,
+                        constraints: const BoxConstraints(),
+                        onPressed: () async {
+                          final picture = await takePicture();
+                          if (!context.mounted){
+                            return;
+                          }
+                          _navigateBack(context, picture);
+                        },
+                      ),
+                    ),
+                    const Spacer()
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
       ),
-      body: const Center(child: Text('Camara'),)
     );
   }
 
+  _navigateBack(BuildContext context, Uint8List? picture) {
+    Navigator.pop(context, picture);
+  }
 
 }
